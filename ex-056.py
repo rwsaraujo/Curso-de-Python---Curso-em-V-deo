63fd5c1c47ad3de685c2fbc64b1ae086770e12eb
Raphael Araújo <araujowebdev@gmail.com>
2020-06-14 18:56:20 -0300

    nenhuma mudança no código. apenas um teste de atualização na contribuição no Github

--- ex-056.py
+++ ex-056.py
@@ -13,6 +13,7 @@ médiaidade = 0
 maioridadehomem = 0
 nomevelho = ' '
 totmulher20 = 0
+
 for p in range(1, 5):
     print(f'----- {p}ª PESSOA -----')
     nome = str(input('Nome: ')).strip()
@@ -27,6 +28,7 @@ for p in range(1, 5):
         nomevelho = nome
     if sexo in 'Ff' and idade < 20:
         totmulher20 + 1
+        
 médiaidade = somaidade / 4
 print(f'A média de idade do grupo é de {médiaidade}.')
 print(f'O homem mais velho tem {maioridadehomem} anos e se chama {nomevelho}.')
